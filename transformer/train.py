e1cf338d117738181df98325114f8fe7a4241b21
Hanson <105476@grundfos.com>
2024-04-15 16:50:17 +0800

    1

--- transformer/train.py
+++ transformer/train.py
@@ -1,7 +1,13 @@
+import os
+import pickle
+
 import torch
 from torch import nn, optim
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 
 from model import Transformer
+from preprocessing import prepare_lang
+
 
 src_vocab_size = 5000
 tgt_vocab_size = 5000
@@ -23,13 +29,27 @@ transformer = Transformer(
     dropout,
 )
 
-# Generate random sample data
-# (batch_size, seq_length)
-src_data = torch.randint(1, src_vocab_size, (64, max_seq_length))
-# (batch_size, seq_length)
-tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))
+# # Generate random sample data
+# # (batch_size, seq_length)
+# src_data = torch.randint(1, src_vocab_size, (64, max_seq_length))
+# # (batch_size, seq_length)
+# tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))
 
-# print(src_data.size(), tgt_data.size())
+# # print(src_data.size(), tgt_data.size())
+
+with open(os.path.join("..", "data", "eng-deu.pkl"), "rb") as f:
+    raw_data = pickle.load(f)
+
+input_lang, output_lang, input_encoded, target_encoded = prepare_lang(raw_data)
+
+
+train_data = TensorDataset(
+    torch.LongTensor(input_encoded), torch.LongTensor(target_encoded)
+)
+
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=32)
+
 
 criterion = nn.CrossEntropyLoss(ignore_index=0)
 optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
@@ -37,17 +57,23 @@ optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), e
 transformer.train()
 
 for epoch in range(100):
-    optimizer.zero_grad()
-    # output = transformer(src_data, tgt_data[:, :-1])
-    # loss = criterion(
-    #     output.contiguous().view(-1, tgt_vocab_size),
-    #     tgt_data[:, 1:].contiguous().view(-1),
-    # )
-    output = transformer(src_data, tgt_data)
-    loss = criterion(
-        output.contiguous().view(-1, tgt_vocab_size),
-        tgt_data.contiguous().view(-1),
-    )
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+    for input_tensor, target_tensor in train_dataloader:
+
+        optimizer.zero_grad()
+        # output = transformer(src_data, tgt_data[:, :-1])
+        # loss = criterion(
+        #     output.contiguous().view(-1, tgt_vocab_size),
+        #     tgt_data[:, 1:].contiguous().view(-1),
+        # )
+        output = transformer(input_tensor, target_tensor)
+        loss = criterion(
+            output.contiguous().view(-1, tgt_vocab_size),
+            target_tensor.contiguous().view(-1),
+        )
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+
+        if epoch % 10 == 0:
+            # save model to local file
+            torch.save(transformer.state_dict(), f"transformer_{epoch}.pth")
